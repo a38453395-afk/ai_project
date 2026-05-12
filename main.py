e2d58cf4d3e0174d7d0d0c1d7f39bbc772f49787
a38453395-afk <a38453395@gmail.com>
2026-05-12 16:12:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ b=st.selectbox('좋아하는 음식을 선택하세요',['마라탕','피자','�
 if st.button('인사말 생성'):
   st.write(a+'님.안녕하세요')
   st.info(b+'를 좋아 하시는군요!')
+  st.warning('난그거 싫어함ㅋ')
+  st.error('반가워요!')
+  st.baiioons()
+  
