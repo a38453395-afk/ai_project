d51505d93edf3bdbe114f3286b51da00605be582
a38453395-afk <a38453395@gmail.com>
2026-06-02 15:55:10 +0900

    Update 06_수행평가.py

--- pages/06_수행평가.py
+++ pages/06_수행평가.py
@@ -80,3 +80,24 @@ if nickname:
         deck_df,
         use_container_width=True
     )
+st.markdown("""
+<style>
+
+.stApp{
+background-color:#0d1117;
+}
+
+[data-testid="stMetric"]{
+background:#161b22;
+padding:20px;
+border-radius:15px;
+box-shadow:0 0 15px rgba(0,255,255,0.3);
+}
+
+h1{
+color:white;
+text-align:center;
+}
+
+</style>
+""",unsafe_allow_html=True)
