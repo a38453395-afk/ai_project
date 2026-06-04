077e77b4dc6a906e1621a0af8fa899bb03d9a8db
a38453395-afk <a38453395@gmail.com>
2026-06-04 14:10:47 +0900

    Update 06_수행평가.py

--- pages/06_수행평가.py
+++ pages/06_수행평가.py
@@ -1,8 +1,22 @@
 import streamlit as st
+import requests
 
-API_KEY = st.secrets.get("RIOT_API_KEY")
+API_KEY = st.secrets["RIOT_API_KEY"]
 
-if API_KEY:
-    st.success("✅ API 키 연결 성공!")
-else:
-    st.error("❌ API 키를 찾을 수 없음")
+headers = {
+    "X-Riot-Token": API_KEY
+}
+
+st.title("Riot API 테스트")
+
+game_name = st.text_input("게임명")
+tag_line = st.text_input("태그")
+
+if st.button("조회"):
+
+    url = f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
+
+    r = requests.get(url, headers=headers)
+
+    st.write("상태코드:", r.status_code)
+    st.json(r.json())
