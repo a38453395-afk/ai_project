f692366e07ca52a2c2867c1f5206f1891a9b2091
a38453395-afk <a38453395@gmail.com>
2026-06-05 12:43:56 +0900

    Update 06_수행평가.py

--- pages/06_수행평가.py
+++ pages/06_수행평가.py
@@ -1,12 +1,6 @@
 import streamlit as st
-import requests
 
-API_KEY = st.secrets["RIOT_API_KEY"]
+key = st.secrets["RIOT_API_KEY"]
 
-r = requests.get(
-    "https://kr.api.riotgames.com/lol/status/v4/platform-data",
-    headers={"X-Riot-Token": API_KEY}
-)
-
-st.write("상태코드 =", r.status_code)
-st.write(r.text)
+st.write("RGAPI 시작 여부:", key.startswith("RGAPI-"))
+st.write("길이:", len(key))
